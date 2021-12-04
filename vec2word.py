ee6986e96389ff04ed7f89698252c305baac251f
Logan Kearsley <chronosurfer@gmail.com>
2021-12-04 10:44:59 -0700

    Sort by centrality

--- vec2word.py
+++ vec2word.py
@@ -1,12 +1,12 @@
 import sys
 import random
+import operator
 import numpy as np
 from functools import reduce
 from itertools import product
 from gensim.models import KeyedVectors
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
-from collections import defaultdict
 
 def select(k, n):
     value = list(range(k))
@@ -37,8 +37,6 @@ D = len(dims)
 K = reduce(lambda a,b: a*b, dims)
 
 d_order = np.lexsort((dims,))
-print(dims)
-print(d_order)
 
 model_file = sys.argv[1]
 wv = KeyedVectors.load_word2vec_format(model_file, binary=(model_file.endswith('.bin')))
@@ -48,43 +46,47 @@ vector_count = len(wv.vectors)
 if sample_count > vector_count:
     sample_count = vector_count
 
-print("Using", sample_count, "of", vector_count, "vectors for clustering")
+print("Used", sample_count, "of", vector_count, "vectors for clustering")
 samples = [wv.vectors[i,:] for i in select(sample_count, vector_count)]
 
 km = KMeans(n_clusters=K)
-km.fit(wv.vectors)
-centers = km.cluster_centers_
-print("Got Centers")
+
+if cluster_size == 0:
+    labels2words = [list() for _ in range(K)]
+    labels = km.fit_predict(wv.vectors)
+    centers = km.cluster_centers_
+    for i, l in enumerate(labels):
+        w = wv.index_to_key[i]
+        if w is not None:
+            labels2words[l].append(w)
+else:
+    labels2words = []
+    km.fit(wv.vectors)
+    centers = km.cluster_centers_
+    for center in centers:
+        cluster = [r[0] for r in wv.most_similar(positive=[center], topn=(cluster_size+1)) if r[0] is not None]
+        if len(cluster) > cluster_size: cluster.pop()
+        labels2words.append(cluster)
 
 pca = PCA(n_components=D)
 result = pca.fit_transform(centers)
 r_order = np.lexsort((result[:,d_order]).T)
-print("Sorted Centers")
-
-labels2words = defaultdict(list)
-if cluster_size == 0:
-    labels = km.predict(wv.vectors)
-    for i, l in enumerate(labels):
-        labels2words[l].append(wv.index_to_key[i])
-    print("Labelled all vectors")
 
 print("Forms:", K)
 
 for i, emes in enumerate(product(*segments)):
     center = centers[r_order[i],:]
+    cluster = labels2words[r_order[i]]
     if cluster_size == 0:
-        v1, v2 = wv.most_similar(positive=[center], topn=2)
-        word = v1[0] if v1[0] is not None else v2[0]
-        label = r_order[i]
-        cluster = labels2words[label]
-    else:
-        cluster = [r[0] for r in wv.most_similar(positive=[center], topn=cluster_size)]
-        word = cluster[0] if cluster[0] is not None else cluster[1]
+        distances = wv.distances(center, cluster)
+        cluster = [k for k, _ in sorted(zip(cluster, distances), key=operator.itemgetter(1))]
+
+    word = cluster[0]
     print(
         "".join(emes), ':',
-        repr(word.encode("utf-8", errors='replace'))[2:-1] if word is not None else "?",
+        repr(word.encode("utf-8", errors='replace'))[2:-1],
         len(cluster)
     )
-    for w in sorted(w for w in cluster if w is not None and w != word):
+    for w in cluster[1:]:
         print('\t', repr(w.encode("utf-8", errors='replace'))[2:-1])
     print('==========')
